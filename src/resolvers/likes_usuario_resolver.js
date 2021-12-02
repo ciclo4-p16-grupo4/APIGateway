7c824c3e832a481295edd69869018d3b2d9348cc
yaderr <yadersonjesuspatinovidal@gmail.com>
2021-12-02 18:20:48 -0500

    Adding likes user deletre

--- src/resolvers/likes_usuario_resolver.js
+++ src/resolvers/likes_usuario_resolver.js
@@ -3,19 +3,14 @@ const { ApolloError } = require('apollo-server')
 const likesUserResolver = {
     Query: {
         likesByInmueble: async (_, { inmueble_id }, { dataSources, userIdToken }) => {
-            usernameToken = (await dataSources.authAPI.getUser(userIdToken)).username
-            if (username == usernameToken)
             return await dataSources.likesAPI.getLikesByInmueble(inmueble_id);
-            else
-               return null;
         },
 
         likeByUser: async (_, {user_id}, {dataSources, userIdToken}) => {
-            usernameToken = (await dataSources.authAPI.getUser(userIdToken)).username
-            if (username == usernameToken)
+            if (userIdToken == user_id)
                 return await dataSources.likesAPI.getLikesByUser(user_id);
             else
-               return null; 
+                throw new ApolloError("NO AUTORIZADO", 401)
         },
 
         likesAll: async (_, {}, {dataSources, userIdToken}) => {
@@ -32,20 +27,23 @@ const likesUserResolver = {
     },
     Mutation: {
         createLike: async (_, {like}, {dataSources, userIdToken}) => {
-            usernameToken = (await dataSources.authAPI.getUser(userIdToken)).username
-            if (username == usernameToken)
+            if (userIdToken == like.user_id)
                 return await dataSources.likesAPI.createLike(like);
             else
-               return null;
-
+                throw new ApolloError("NO AUTORIZADO", 401)
+            
         },
 
         deleteLike: async (_, {likeId}, {dataSources, userIdToken}) => {
-            usernameToken = (await dataSources.authAPI.getUser(userIdToken)).username
-            if (username == usernameToken)
-                return await dataSources.likesAPI.deleteLike(likeId);
-            else
-               return null;
+            if (!userIdToken) throw new ApolloError("NO AUTORIZADO INICIE SESIÓN", 401)
+
+            const user = await dataSources.authAPI.getUser(userIdToken)
+
+            if (!user.is_staff) {
+                throw new ApolloError("NO AUTORIZADO", 401)
+            }
+
+            return await dataSources.likesAPI.deleteLike(likeId);
         }
 
     }
