fd73de56912c73816e62e61fdc166e5b66dd557c
Jimmy P <jimmyporinsu@gmail.com>
2021-12-02 01:09:23 -0500

    corrección index.js typeDefs

--- api-gateway/src/typeDefs/index.js
+++ api-gateway/src/typeDefs/index.js
@@ -1,7 +1,7 @@
 const authTypeDefs = require('./auth_type_def');
 const inmueblesTypeDefs = require('./inmuebles_type_def');
 const likescountTypeDefs = require('./likes_count_type_def');
-const likesusuarioTypeDefs = rquiere('./likes_usuario_type_def');
+const likesusuarioTypeDefs = require('./likes_usuario_type_def');
 //Se unen
 const schemasArrays = [authTypeDefs, inmueblesTypeDefs, likescountTypeDefs, likesusuarioTypeDefs];
 //Se exportan
